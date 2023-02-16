5b822850129d43cf4247a95be0382dfe02809563
GranciMuggli <grancichan@outlook.com>
2023-02-16 11:26:50 +0800

    new
    create a new file

--- people/chenguanqing.py
+++ people/chenguanqing.py
@@ -1,5 +1,7 @@
+# Muggle'GD
+# 2023/2/3
 import numpy as np
-
-vector=numpy.array([5,6,7,8])
-
-print(vector)
+arr = np.random.randint(0,10,size=10)
+mean = np.mean(arr)
+print("array",arr)
+print("average value",mean)
