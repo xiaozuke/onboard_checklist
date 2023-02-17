93d83cb0d7d3cbf04a22caf08f415a1dbefc3949
GranciMuggli <grancichan@outlook.com>
2023-02-17 12:48:01 +0800

    delete

--- people/chenguanqing.py
+++ people/chenguanqing.py
@@ -1,7 +1,7 @@
 # Muggle'GD
 # 2023/2/3
 import numpy as np
-arr = np.random.randint(0,10,size=10)
+arr = np.random.randint(0, 10, size=10)
 mean = np.mean(arr)
-print("array",arr)
-print("average value",mean)
+print("array", arr)
+print("average value", mean)
